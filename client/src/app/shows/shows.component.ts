9e174937c0c6d3d82aa67795d7c79bb6c823af96
Alex Myers <amyers25@students.towson.edu>
2017-05-10 20:39:18 -0400

    Build Shows Component

---
+++ client/src/app/shows/shows.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../models/Employee';
+import { EmployeeService } from './employee.service';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html'
+})
+export class EmployeeComponent implements OnInit {
+
+  employees: Employee[];
+  editID: number = -1;
+
+  constructor(public employeeService: EmployeeService) { 
+    this.employees = employeeService.get();
+  }
+
+  ngOnInit() {
+  }
+
+  edit(index: number) {
+    this.editID = index;
+  }
+
+  save(index: number) {
+    this.editID = -1;
+    this.employeeService.update(this.employees[index]);
+  }
+
+  remove(index: number) {
+    this.employees.splice(index, 1);
+    this.employeeService.remove(this.employees[index]);
+  }
+
+}
