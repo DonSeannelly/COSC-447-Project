f301792ba4b67a47f7e6cc3c2f9173716f9b95a3
Sean Donnelly <sdonnelly3000@gmail.com>
2017-05-10 22:54:05 -0400

    Fix Employee Service
    The service now follows the pattern of the previous commit.

--- client/src/app/employee/employee.service.ts
+++ client/src/app/employee/employee.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
 import 'rxjs/add/operator/map'
 import {serviceInterface} from '../services/serviceInterface';
+import {Employee} from "../models/Employee";
 
 @Injectable()
 export class EmployeeService implements serviceInterface {
@@ -13,25 +14,37 @@ export class EmployeeService implements serviceInterface {
         this.http.get('/api/ticket')
             .map(res => res.json()).subscribe(response => {
             for (let temp of response) {
-                result[count] = {employeeID: temp[0].value, phone: temp[1].value, fname: temp[2].value,
-                    lname: temp[3].value, minit: temp[4].value, zip: temp[5].value, zip: temp[6].value, 
-					state: temp[7].value, city: temp[8].value, street: temp[9].value};
+                result[count] = {employeeID: temp[0].value, position: temp[1].value, phone: temp[2].value, firstName: temp[3].value,
+                  lastName: temp[4].value, middleInitial: temp[5].value, zip: temp[6].value, state: temp[7].value, city: temp[8].value, street: temp[9].value};
                 count++;
             }
         });
         return result;
     }
-  create(record) {
-    return this.http.get('/api/employee', record)
+  create(record: Employee) {
+    return this.http.get('/api/employee', this.mapToSchema(record))
         .map(res => res.json());
   }
   remove(id) {
     return this.http.delete('/api/employee/' + id)
         .map(res => res.json());
   }
-  update(record) {
-    return this.http.put('/api/employee', record)
+  update(record: Employee) {
+    return this.http.put('/api/employee', this.mapToSchema(record))
         .map(res => res.json());
   }
-
+  mapToSchema(record: Employee) {
+    return {
+      Emp_ID: record.employeeID,
+      Position: record.position,
+      Phone: record.phone,
+      Fname: record.firstName,
+      Lname: record.lastName,
+      Minit: record.middleInitial,
+      Zip: record.zip,
+      State: record.state,
+      City: record.city,
+      Street: record.street
+    };
+  }
 }
